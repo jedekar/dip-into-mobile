487a2b4a6ca9b7c315c51668fe01698c2323e77c
Oleksandr Khodos <ax.khodos@gmail.com>
2021-05-11 21:47:31 +0300

    Add movies tab

---
+++ app/src/main/java/ua/kpi/comsys/io8128/ui/movies/MoviesFragment.java
@@ -0,0 +1,38 @@
+package ua.kpi.comsys.io8128.ui.movies;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ua.kpi.comsys.io8128.R;
+
+public class MoviesFragment extends Fragment {
+
+    private MoviesViewModel mViewModel;
+
+    public static MoviesFragment newInstance() {
+        return new MoviesFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movies, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MoviesViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
