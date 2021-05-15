7d02f1d060cbd2b9fea8bf3a77d16e442fe3a924
Oleksandr Khodos <ax.khodos@gmail.com>
2021-05-15 22:17:49 +0300

    Create satisfiable item layout

--- app/src/main/java/ua/kpi/comsys/io8128/ui/movies/MoviesFragment.java
+++ app/src/main/java/ua/kpi/comsys/io8128/ui/movies/MoviesFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.Adapter;
 
@@ -50,6 +51,7 @@ public class MoviesFragment extends Fragment {
         recyclerView = (RecyclerView) root.findViewById(R.id.movies_recycler_view);
         adapter = new MoviesAdapter(parseDataset());
         recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
 
         return root;
     }
